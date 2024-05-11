9ad9c0f2952efab6ff23596467e48e06dfc2a3c2
Ndungu-Karen <162780545+Ndungu-Karen@users.noreply.github.com>
2024-05-11 22:47:17 +1000

    create User, Product and CartItem model

---
+++ models/Product.js
@@ -0,0 +1,50 @@
+// models/Product.js
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  current_price: {
+    type: Number,
+    required: true
+  },
+  previous_price: {
+    type: Number,
+    required: true
+  },
+  product_details:{
+    type: [String],
+    required:true
+  },
+  images: {
+    type: [String], // Array of image URLs
+    required: true
+  },
+  sizes: {
+    type: [String],
+    required: true
+  },
+  colors: {
+    type: [String],
+    required: true
+  },
+  rating:{
+    type:Number,
+    required: true
+  },
+  category: {
+    type: String,
+    enum: ['women', 'footwear', 'kids', 'accessories', 'mens', 'unisex'], // Enumerated list of allowed categories
+    required: true
+  }
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
