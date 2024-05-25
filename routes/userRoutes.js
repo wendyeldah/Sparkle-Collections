ea8a1c67df661fc53b2ed882ba021fe9d5642c13
Ndungu-Karen <162780545+Ndungu-Karen@users.noreply.github.com>
2024-05-26 00:16:10 +1000

    update user route to handle user login

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,15 +5,6 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
 
-// Import the isLoggedIn middleware
-const { isLoggedIn } = require('../controllers/authController');
-
-router.get('/data', isLoggedIn, async (req, res) => {
-    // Check if user is logged in
-    // If isLoggedIn middleware allows the request to reach here, it means the user is authenticated
-    // You can then fetch user data from the database and send it in the response
-});
-
 // Create a new user
 router.post('/signup', userController.signup);
 
